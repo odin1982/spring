bc55af582290d1cbe6facba43e2f74d9863154d8
odin-huawei <odin.araujo.barragan@gmail.com>
2021-05-15 01:55:14 -0500

    Declaring a simple bean

--- src/main/java/spring/pag040/Test.java
+++ src/main/java/spring/pag040/Test.java
@@ -4,10 +4,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws PerformanceException {
 		//ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-idol.xml");
 		ApplicationContext ctx = new FileSystemXmlApplicationContext("C:\\Users\\odina\\Documents\\eclipse-workspace\\spring\\spring\\src\\main\\resources\\spring-idol.xml");
 		Performer performer = (Performer)ctx.getBean("duke");
+		performer.perform();
 	}
 
 }
