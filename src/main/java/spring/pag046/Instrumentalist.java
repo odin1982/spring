6ec3082b4fe123ae70996194161963b34f59d6ad
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2021-05-24 22:14:47 -0500

    Injecting Simple Values

--- src/main/java/spring/pag046/Instrumentalist.java
+++ src/main/java/spring/pag046/Instrumentalist.java
@@ -5,18 +5,24 @@ import spring.pag040.Performer;
 
 public class Instrumentalist implements Performer{
 	private String song;
+	private int age;
 	private Instrument instrument;
-	
 	
 	public Instrumentalist() {}
 
 	@Override
 	public void perform() throws PerformanceException {
-		System.out.println("Playing "+ song + " : ");
+		System.out.println("Playing : " + song + "with age: " + age);
 	}
 
 	public void setSong(String song) {
 		this.song = song;
+	}
+	
+	
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public void setInstrument(Instrument instrument) {
