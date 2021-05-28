02cbe71ccbb95126fc596d0db9deab699a20f367
odin <odin@joker>
2021-05-28 08:13:29 -0500

    Se agrega ubicación application context en linux

--- src/main/java/spring/pag040/Test.java
+++ src/main/java/spring/pag040/Test.java
@@ -6,7 +6,8 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class Test {
 	public static void main(String[] args) throws PerformanceException {
 		//ApplicationContext ctx = new FileSystemXmlApplicationContext("C:\\Users\\odin\\Documents\\workspace-eclipse\\spring\\spring\\src\\main\\resources\\spring-idol.xml");
-		ApplicationContext ctx = new FileSystemXmlApplicationContext("C:\\Users\\odina\\Documents\\eclipse-workspace\\spring\\spring\\src\\main\\resources\\spring-idol.xml");
+		//ApplicationContext ctx = new FileSystemXmlApplicationContext("C:\\Users\\odina\\Documents\\eclipse-workspace\\spring\\spring\\src\\main\\resources\\spring-idol.xml");
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("/src/main/resources/spring-idol.xml");
 		
 		//A simple bean
 		Performer odin = (Performer)ctx.getBean("odin");
