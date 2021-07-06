76848ae48837d5e2cf32954a9b326b7b4d7d77b0
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-05 21:44:27 -0500

    Defined a bean explicity with @Bean

--- src/main/java/org/spring/config/AppConfig.java
+++ src/main/java/org/spring/config/AppConfig.java
@@ -1,7 +1,13 @@
 package org.spring.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //APUNTES-@Configuration-This declares it as a Spring configuration class
@@ -10,5 +16,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = {"org.spring.controller"})
 public class AppConfig {
+	@Bean(name="theSource")
+	//To force to be executed each time it's called (and return a different object dataSource() each time), use the annotation with a scope @Scope prototype
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();//Se agrego dependencia spring jdbc
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/orderapi");
+		dataSource.setUsername("root");
+		dataSource.setPassword("barcelona8");
+		return dataSource;
+	}
 
 }
