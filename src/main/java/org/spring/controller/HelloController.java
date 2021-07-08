5a4a081bdf8c839c71039de035542efb8d536e49
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-07 21:43:12 -0500

    Using a bean directly

--- src/main/java/org/spring/controller/HelloController.java
+++ src/main/java/org/spring/controller/HelloController.java
@@ -2,6 +2,7 @@ package org.spring.controller;
 
 import org.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,9 @@ public class HelloController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private ApplicationContext context;
 
 	@RequestMapping("hi")
 	@ResponseBody
@@ -23,5 +27,15 @@ public class HelloController {
 	@ResponseBody
 	public String hiUsers() {
 		return "nb of users: " + userService.findNumberOfUsers();
+	}
+	
+	//Using a bean directly
+	@RequestMapping("bean-directly")
+	@ResponseBody
+	public String usingBeanDirectly() {
+		UserService bean = (UserService)context.getBean("userService");
+		//another way to get a bean by its class
+		//UserService bean2 = context.getBean(UserService.class);
+		return "number of users: " + bean.findNumberOfUsers();
 	}
 }
