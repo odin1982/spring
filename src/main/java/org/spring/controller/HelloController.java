5c935636e9b70398f7156716125ce11273c3de95
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-07 21:22:32 -0500

    Using a bean via dependency injection with @Autowired

--- src/main/java/org/spring/controller/HelloController.java
+++ src/main/java/org/spring/controller/HelloController.java
@@ -1,15 +1,27 @@
 package org.spring.controller;
 
+import org.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("hi")
 	@ResponseBody
 	public String hi() {
 		return "Hello, world";
+	}
+	
+	
+	@RequestMapping("hiUsers")
+	@ResponseBody
+	public String hiUsers() {
+		return "nb of users: " + userService.findNumberOfUsers();
 	}
 }
