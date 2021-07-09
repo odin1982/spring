50407d8213e8602b7d386226b9b50611e3719211
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-08 21:48:55 -0500

    Associate a Route with a Controller method

---
+++ src/main/java/org/spring/controller/UserController.java
@@ -0,0 +1,12 @@
+package org.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@RequestMapping("/user/list")
+	public void userList() {
+		
+	}
+}
