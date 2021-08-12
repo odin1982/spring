f347db461032ad126dc082f319330aaf7adb1d7b
odin-huawei <odin.araujo.barragan@gmail.com>
2021-08-12 00:18:29 -0500

    Declaring beans

---
+++ src/main/java/spring/pag031/PerformanceException.java
@@ -0,0 +1,5 @@
+package spring.pag031;
+
+public class PerformanceException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/spring/pag031/Performer.java
@@ -0,0 +1,5 @@
+package spring.pag031;
+
+public interface Performer {
+	void perform() throws PerformanceException;
+}
