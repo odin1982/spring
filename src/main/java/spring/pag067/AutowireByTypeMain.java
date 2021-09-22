48de830bf44a1fe48a9b0ebdc7132b8aea60b397
odin-huawei <odin.araujo.barragan@gmail.com>
2021-09-22 08:11:51 -0500

    Autowiring byType

---
+++ src/main/java/spring/pag067/AutowireByTypeMain.java
@@ -0,0 +1,15 @@
+package spring.pag067;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.pag031.PerformanceException;
+import spring.pag031.Performer;
+
+public class AutowireByTypeMain {
+	public static void main(String[] args) throws PerformanceException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appcontext-autowiring-bytype.xml");
+		
+		Performer kenny3 = (Performer)context.getBean("kenny3");
+		kenny3.perform();
+	}
+}
